90ddc176c4b7204a697de1b4c6eef81e5507a18c
amalina-coffee <55571716+amalina-coffee@users.noreply.github.com>
2020-12-10 16:12:28 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -29,7 +29,7 @@
 <!doctype html>  
 <html lang="en">
     <head>
-        <meta charset="utf-8">
+        <meta charset="utf-8"> 
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>Visit counter</title>
     </head>
