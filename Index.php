c1b1dd043fb8ab6d46b9da20662b59f6f6e636a2
amalina-coffee <55571716+amalina-coffee@users.noreply.github.com>
2020-12-10 15:53:21 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,6 +1,15 @@
-<?php
 
-        echo "no results";
 
-?>
+<!doctype html>  
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Visit counter</title>
+    </head>
+    <body>
+            <?php echo 'Hello World'; ?>
+
+    </body>
+</html>
 
