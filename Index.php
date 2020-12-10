aa14b46195dd7afc6a2f96d1195d30e6a65a05d4
amalina-coffee <55571716+amalina-coffee@users.noreply.github.com>
2020-12-10 17:22:59 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -5,35 +5,66 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-	<title>LAMP App</title>
+	<title>PRACTICE</title>
 	<link rel="stylesheet" href="styles/styles.css">
-	
 </head>
 <body>
-    <h3>LAMP App</h3>
-    <?php
-	require_once("login_db.php");
-    
-    $sql = "UPDATE Counter SET visits = visits+1 WHERE id = 1";
-    $conn->query($sql);
-    $sql = "SELECT visits FROM Counter WHERE id = 1";
-    $result = $conn->query($sql);
+	<h1>LAMP App</h1>
 
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            $visits = $row["visits"];
-        }
-    } 
-    else {
-        echo "no results";
-    }
-    ?>
-    
-    <h3>Visits: <?php echo $visits; ?></h3>
+	<?php
+	require_once("login_db.php");
+	// Add any new users before querying User table
+	$name = $_POST['name'];
+	if(isset($name)) {
+		$query = 'INSERT INTO `sampledb`.`User` (`name`) VALUES ("'.$name.'");';
+
+		if ($conn->query($query) === TRUE) {
+			echo "User created successfully";
+		} else {
+			echo "Error: <br>" . $conn->error;
+		}
+	}
+
+	$query = 'SELECT * FROM User';
+	$result = $conn->query($query);
+
+	if($result) {
+		echo "<table>";
+		while ($row = mysqli_fetch_array($result)) {
+			echo "<tr>";
+			echo "<td>".$row['user_id']."</td>"; 
+			echo "<td>".$row['name']."</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+	} else {
+		echo "Error: The User table might not have been created yet. Query failed: $query";
+	}
+	?>
+
+	<h3>Add New User</h3>
+	<form action="index.php" method="post">
+		Name: <input type="text" id="name" name="name"><br />
+		<input type = "submit" onclick="validateUserInfo()">
+	</form>
+
+	<script>
+		function validateUserInfo() {
+			var name = document.getElementById("name").value;
+			if(name === "") {
+				output += "Name must not be blank.\n";
+			}
+
+			if(output != "") {
+				alert(output);
+			}
+		}
+	</script>
 </body>
 </html>
+
+
 
 <?php
 $conn->close();
 ?>
-     
