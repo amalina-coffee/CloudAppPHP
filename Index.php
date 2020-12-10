932b17729c7e9a970fbcd11d062c1fda01e8d4bd
amalina-coffee <55571716+amalina-coffee@users.noreply.github.com>
2020-12-10 15:45:00 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,40 +1,6 @@
 <?php
-    $servername = "localhost";
-    $username = "dtbsuser";
-    $password = "dtbs#passw01";
-    $dbname = "dtbsname";
-    $conn = new mysqli($servername, $username, $password, $dbname);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
-
-    $sql = "UPDATE Counter SET visits = visits+1 WHERE id = 1";
-    $conn->query($sql);
-
-    $sql = "SELECT visits FROM Counter WHERE id = 1";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            $visits = $row["visits"];
-        }
-    } else {
         echo "no results";
-    }
-    
-    $conn->close();
+
 ?>
 
-<!doctype html>  
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Visit counter</title>
-    </head>
-    <body>
-        Visits: <?php print $visits; ?>
-
-    </body>
-</html>
