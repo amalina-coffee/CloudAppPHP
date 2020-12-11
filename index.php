14792e6cadaf531c752246a9bd6488d4890f2ffb
amalina-coffee <55571716+amalina-coffee@users.noreply.github.com>
2020-12-10 19:12:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,14 +14,23 @@ session_start();
     <?php
 	require_once("login_db.php");
 	
-	$sql = "UPDATE counter SET visits = visits + 1");
+	$sql = "UPDATE Counter SET visits = visits+1";
 	$conn->query($sql);
 	
-	$sql = "SELECT visits FROM counter";
+	$sql = "SELECT visits FROM Counter";
 	$result = $conn->query($sql);
+	
+	if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $visits = $row["visits"];
+	}
+	
+	} else {
+        echo "no results";
+	}
     
     ?>
-    You are visitor #: <?php print $result; ?>
+    You are visitor #: <?php print $visits; ?>
 </body>
 </html>
 
