b8bb00146a81008ebebf7a5297b898ee3bf1a2a1
amalina-coffee <55571716+amalina-coffee@users.noreply.github.com>
2020-12-11 11:22:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -38,10 +38,10 @@ session_start();
 	
 	$name = $_POST['name'];
 	if(isset($name)) {
-		$query = 'INSERT INTO `sampledb`.`User` (`name`) VALUES ("'.$name.'");';
+		$query = 'INSERT INTO User (name) VALUES ('{$name}');';
 
 		if ($conn->query($query) === TRUE) {
-			echo "<br>Hello, " . $name . "You are visitor #: " . $visits; 
+			echo "<br>Hello, " . $name . ". You are visitor #" . $visits; 
 		} else {
 			echo "Error: <br>" . $conn->error;
 		}
